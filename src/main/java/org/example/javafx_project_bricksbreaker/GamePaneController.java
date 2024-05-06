3dd8578c60339e0d77921aaab533cecc6ba9fb6e
Ahmed Salah <ahmed1347salah1347@gmail.com>
2024-05-06 18:32:01 +0300

    Organizing bricks and text nodes with maps

--- src/main/java/org/example/javafx_project_bricksbreaker/GamePaneController.java
+++ src/main/java/org/example/javafx_project_bricksbreaker/GamePaneController.java
@@ -16,9 +16,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class GamePaneController implements Initializable {
 
@@ -37,13 +35,11 @@ public class GamePaneController implements Initializable {
     private double ballRadius = 10;
     private double ballSpeedX = 3;
     private double ballSpeedY = 3;
-    private Rectangle[] bricks;
-    private List<Text> healthTexts = new ArrayList<>();
-    private int[] brickHealth;
+    private List<Map> bricks;
     private int ballsFallen = 0;
     private ArrayList<Circle> balls = new ArrayList<>();
-    private int maxHealth = 30; // Maximum health of a brick
-    private int numBricks = 88; // Total number of bricks
+    private final int MAXHEALTH = 30; // Maximum health of a brick
+    private final int NUMBRICKS = 88; // Total number of bricks
     private int cannonX = 350; // X position of the cannon
     private int cannonY = 720; // Y position of the cannon
     private Rectangle cannon; // Cannon shape
@@ -52,13 +48,12 @@ public class GamePaneController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        initializeBrickHealth(); // Initialize brick health array
         createBricks();
         createCannon();
         sliderangel.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                slidersetangel=(int)sliderangel.getValue();
+                slidersetangel = (int) sliderangel.getValue();
             }
         });
         launchBalls(slidersetangel); // Launch multiple balls from the cannon
@@ -73,12 +68,6 @@ public class GamePaneController implements Initializable {
         timer.start();
     }
 
-    private void initializeBrickHealth() {
-        brickHealth = new int[numBricks];
-        for (int i = 0; i < numBricks; i++) {
-            brickHealth[i] = maxHealth;
-        }
-    }
 
     private void createBall() {
         ball = new Circle(300, 400, ballRadius, Color.LIGHTPINK);
@@ -119,7 +108,7 @@ public class GamePaneController implements Initializable {
                 ballsToRemove.add(ball); // Schedule the ball for removal
                 ballsFallen++;  // Increment the count of fallen balls
             }
-            if (ballsFallen ==numBallsToLaunch) {
+            if (ballsFallen == numBallsToLaunch) {
                 repositionBricks();
             }
             // Update the velocity after handling collisions
@@ -139,8 +128,6 @@ public class GamePaneController implements Initializable {
     }
 
 
-
-
     private void createBricks() {
         int brickSize = 50; // Size of each brick (width and height)
         int spacing = 5; // Spacing between bricks
@@ -149,7 +136,8 @@ public class GamePaneController implements Initializable {
         int startX = 50; // Starting X position
         int startY = 50; // Starting Y position
 
-        bricks = new Rectangle[numBricks];
+        bricks = new ArrayList<>();
+
 
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
@@ -182,20 +170,26 @@ public class GamePaneController implements Initializable {
                             brick.setArcHeight(20);
                             break;
                     }
-                    root.getChildren().add(brick);
-                    brick.setId(Integer.toString(index));
-
-                    bricks[index] = brick;
 
                     // Create text node for displaying health
-                    Text healthText = new Text(Integer.toString(brickHealth[index]));
-                    healthText.setId(Integer.toString(index));
+                    Text healthText = new Text(Integer.toString(MAXHEALTH));
                     healthText.setFont(Font.font("Arial", FontWeight.BOLD, 14));
                     healthText.setFill(Color.WHITE);
-                    healthText.setX(x + brickSize / 2 - 5); // Center text horizontally
-                    healthText.setY(y + brickSize / 2 + 5); // Center text vertically
+                    healthText.setX(x + (double) brickSize / 2 - 5); // Center text horizontally
+                    healthText.setY(y + (double) brickSize / 2 + 5); // Center text vertically
+                    root.getChildren().add(brick);
                     root.getChildren().add(healthText); // Add text to AnchorPane
-                    healthTexts.add(healthText);
+
+                    Map<String, Object> brickMap = new HashMap<>() {{
+                        put("brick", brick);
+                        put("text", healthText);
+                        put("health", MAXHEALTH);
+                        put("id", index);
+                    }};
+                    bricks.add(brickMap);
+                } else {
+                    Map<String, Object> brick = new HashMap<>();
+                    bricks.add(brick);
                 }
             }
         }
@@ -212,12 +206,14 @@ public class GamePaneController implements Initializable {
         int index = row * 11 + col;
         return design.charAt(index) == '#';
     }
+
     private void createCannon() {
         // Create cannon shape
         cannon = new Rectangle(cannonX - ballRadius / 2, cannonY, ballRadius, ballRadius * 2);
         cannon.setFill(Color.GRAY);
         root.getChildren().add(cannon);
     }
+
     private void launchBalls(int angel) {
         AnimationTimer timer = new AnimationTimer() {
             private long lastUpdate = 0; // Track the time of the last ball launch
@@ -250,52 +246,43 @@ public class GamePaneController implements Initializable {
 
     private void repositionBricks() {
         int deltaY = 20;  // Amount to move bricks down
-        for (Rectangle brick : bricks) {
-            if (brick != null) {
-                brick.setY(brick.getY() + deltaY);
-            }
-        }
-        for (Text text : healthTexts) {
-            text.setY(text.getY() + deltaY);
-        }
-    }
-
-
-
-
-    private void updateHealthText(int index) {
-        // Find the health text node associated with the given brick index
-        for (Node node : root.getChildren()) {
-            if (node instanceof Text healthText) {
-                if (Integer.parseInt(healthText.getId()) == index) {
-                    if (brickHealth[index] == 0) healthText.setVisible(false);
-                    // Update the health text with the new health value
-                    healthText.setText(Integer.toString(brickHealth[index]));
-                    break; // Exit loop once the health text is found and updated
-                }
-            }
+        for (Map brickMap : bricks) {
+            if (brickMap.isEmpty()) continue;
+            Rectangle brick = (Rectangle) brickMap.get("brick");
+            Text healthText = (Text) brickMap.get("text");
+            brick.setY(brick.getY() + deltaY);
+            healthText.setY(healthText.getY() + deltaY);
         }
     }
+
+
 
 
     private void checkBrickCollision(Circle ball, double[] velocity) {
         double vx = velocity[0];
         double vy = velocity[1];
 
-        for (int i = 0; i < numBricks; i++) {
-            if (brickHealth[i] > 0 && bricks[i] != null && ball.getBoundsInParent().intersects(bricks[i].getBoundsInParent())) {
-                brickHealth[i]--;
-                updateHealthText(i);
-                if (brickHealth[i] == 0) {
-                    bricks[i].setVisible(false);  // Hide the brick if its health is depleted
+        for (int i = 0; i < NUMBRICKS; i++) {
+            if (bricks.get(i).isEmpty()) continue;
+            Map brickMap = bricks.get(i);
+            Rectangle brick = (Rectangle) brickMap.get("brick");
+            int brickHealth = (int) brickMap.get("health");
+            Text brickText = (Text) brickMap.get("text");
+            if (brickHealth > 0 && ball.getBoundsInParent().intersects(brick.getBoundsInParent())) {
+                brickMap.put("health", brickHealth - 1);
+                brickText.setText(String.valueOf(brickHealth));
+                // casting health again to get updated value
+                if ((int) (brickMap.get("health")) == 0) {
+                    brick.setVisible(false); // Hide the brick if its health is depleted
+                    brickText.setVisible(false); // Hide brick text if health is depleted
                 }
 
                 double ballCenterX = ball.getCenterX();
                 double ballCenterY = ball.getCenterY();
-                double brickTop = bricks[i].getY();
-                double brickBottom = brickTop + bricks[i].getHeight();
-                double brickLeft = bricks[i].getX();
-                double brickRight = brickLeft + bricks[i].getWidth();
+                double brickTop = brick.getY();
+                double brickBottom = brickTop + brick.getHeight();
+                double brickLeft = brick.getX();
+                double brickRight = brickLeft + brick.getWidth();
 
                 // Check which side of the brick the ball has hit and reverse the appropriate velocity component
                 if (ballCenterX > brickLeft && ballCenterX < brickRight) {
